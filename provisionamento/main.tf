4f82e9b7fe709ff4998f1ff13ffb8d6310102a98
DierlysRodrigues <dierlysrodrigues@hotmail.com>
2021-07-31 13:01:31 -0300

    Adicionado pasta provisionamento com arquivos .tf do terraform

---
+++ provisionamento/main.tf
@@ -0,0 +1,67 @@
+#Configuração Conexão Microsoft Azure
+provider "azurerm" {
+    subscription_id = var.subscription_id
+    client_id = var.client_id
+    client_secret = var.client_secret
+    tenant_id = var.tenant_id
+    features {}
+}
+#Criação Grupo de Recursos e Localização
+resource "azurerm_resource_group" "terraformrg" {
+  name     = "terraform-unyleya"
+  location = "eastus"
+}
+#Criação Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space     = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.terraformrg.location
+  resource_group_name = azurerm_resource_group.terraformrg.name  
+}
+#Criação Sub-Rede
+resource "azurerm_subnet" "integrationsubnet" {
+  name                 = "integrationsubnet"
+  resource_group_name  = azurerm_resource_group.terraformrg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+#Criação IP Publico AzureRM
+resource "azurerm_public_ip" "terraformpublicip" {
+  name                = "APP"
+  location            = azurerm_resource_group.terraformrg.location
+  resource_group_name = azurerm_resource_group.terraformrg.name
+  allocation_method   = "Dynamic"
+}
+resource "azurerm_app_service_plan" "appplanunyleya" {
+  name                = "unyleya-appserviceplan"
+  location            = azurerm_resource_group.terraformrg.location
+  resource_group_name = azurerm_resource_group.terraformrg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+#Criação App Service
+resource "azurerm_app_service" "appserviceunyleya" {
+  name                = "unyleya-app-service"
+  location            = azurerm_resource_group.terraformrg.location
+  resource_group_name = azurerm_resource_group.terraformrg.name
+  app_service_plan_id = azurerm_app_service_plan.appplanunyleya.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+
+}
---
+++ provisionamento/vars.tf
@@ -0,0 +1,20 @@
+#ID Subscription Azure
+variable "subscription_id" {
+  type = string
+  default = "value"
+}
+#ClientID do App
+variable "client_id" {
+  type = string
+  default = "value"
+}
+#Secret gerada no Azure
+variable "client_secret" {
+  type = string
+  default = "value"
+}
+#Tenant ID
+variable "tenant_id" {
+  type = string
+  default = "value"
+}
